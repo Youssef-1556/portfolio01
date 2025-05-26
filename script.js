3867649e15c7da92a74115cfda15b64497589f5f
Youssef-1556 <y.ihihi2005@gmail.com>
2025-05-26 13:15:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleDarkMode() {
+  document.body.classList.toggle('dark');
+  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+}
+
+window.onload = () => {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+  }
+};
